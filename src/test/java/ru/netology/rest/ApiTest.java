6c5c6076651f4871df70243e609d454cc41bc2c0
Natalya Makarevich <mak.nathalie@gmail.com>
2021-10-24 18:55:49 +0300

    Fixed test

--- src/test/java/ru/netology/rest/ApiTest.java
+++ src/test/java/ru/netology/rest/ApiTest.java
@@ -19,6 +19,6 @@ class ApiTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("data", equalTo("some data"));
     }
 }
